5c50cca9e372a683d3aa5b916aed2b1d144f633c
andrianrf <andrian.febriana@multipolar.com>
2023-07-14 17:26:47 +0700

    add jmeter

--- src/test/java/com/multipolar/vagateway/VaGatewayApplicationTests.java
+++ src/test/java/com/multipolar/vagateway/VaGatewayApplicationTests.java
@@ -3,11 +3,11 @@ package com.multipolar.vagateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class VaGatewayApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class VaGatewayApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
