bd2a9543beaf5a8d1cbd74636ced82108c36aa1f
andrianrf <andrian.febriana@multipolar.com>
2023-11-02 20:07:02 +0700

    add function timeout to testController

--- src/main/java/com/multipolar/vagateway/modules/rootController/rootController.java
+++ src/main/java/com/multipolar/vagateway/modules/rootController/rootController.java
@@ -1,7 +1,9 @@
 package com.multipolar.vagateway.modules.rootController;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,20 +18,25 @@ import java.util.Map;
 @RequestMapping("/")
 public class rootController {
 
-
-    @Value("${default.message.test}")
-    private String defaultMessageTest;
+    @Autowired
+    Environment env;
 
     @GetMapping("/test")
     public ResponseEntity<?> test(@RequestParam(defaultValue = "") String message, HttpServletRequest request) {
 
         if(message.equals("")){
-            message = defaultMessageTest;
+            message = env.getProperty("test.default.message");
         }
 
         Map<String, Object> map = new HashMap<>();
         map.put("message", message);
         map.put("requestDate", new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date()));
+        log.info("Client IP : "+request.getRemoteAddr()+", Request : GET /test?message="+message);
+        try {
+        Thread.sleep(Integer.valueOf(env.getProperty("test.timeout.second")) * 1000);
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+        }
         log.info("Client IP : "+request.getRemoteAddr()+", Request : GET /test?message="+message+", Response : "+map);
         return ResponseEntity.ok().body(map);
     }
